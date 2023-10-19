5346ae26e4e66117cc5b5978031eff8c7558628b
Ashwin <ashwin027@gmail.com>
2023-10-18 20:21:37 -0600

    added tracing

--- Program.cs
+++ Program.cs
@@ -1,17 +1,103 @@
-var builder = WebApplication.CreateBuilder(args);
+using aws_dotnet_otlp;
+using OpenTelemetry.Contrib.Extensions.AWSXRay.Trace;
+using OpenTelemetry;
+using OpenTelemetry.Instrumentation.AspNetCore;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System.Diagnostics.Metrics;
+using System.Reflection.PortableExecutable;
 
-// Add services to the container.
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var appBuilder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+        // Add services to the container.
 
-var app = builder.Build();
+        var collectorEndpoint = new Uri(appBuilder.Configuration.GetValue("OpenTelemetry:CollectorEndpoint", defaultValue: "http://localhost:4317")!);
+        Action<ResourceBuilder> configureResource = r => r.AddService(
+            serviceName: appBuilder.Configuration.GetValue("ServiceName", defaultValue: "otel-test")!,
+            serviceVersion: typeof(Program).Assembly.GetName().Version?.ToString() ?? "unknown",
+            serviceInstanceId: Environment.MachineName);
 
-// Configure the HTTP request pipeline.
+        appBuilder.Services.AddSingleton<Instrumentation>();
 
-app.UseHttpsRedirection();
+        appBuilder.Services.AddOpenTelemetry()
+            .ConfigureResource(configureResource)
+            .WithTracing(builder =>
+            {
+                // Tracing
 
-app.UseAuthorization();
+                // Ensure the TracerProvider subscribes to any custom ActivitySources.
+                builder.AddAspNetCoreInstrumentation()
+                    .AddXRayTraceId()
+                    .AddAWSInstrumentation()
+                    .AddHttpClientInstrumentation();
+                    
 
-app.MapControllers();
+                // Use IConfiguration binding for AspNetCore instrumentation options.
+                appBuilder.Services.Configure<AspNetCoreInstrumentationOptions>(appBuilder.Configuration.GetSection("AspNetCoreInstrumentation"));
 
-app.Run();
+                builder.AddOtlpExporter(otlpOptions =>
+                {
+                    // Use IConfiguration directly for Otlp exporter endpoint option.
+                    otlpOptions.Endpoint = collectorEndpoint;
+                });
+
+                Sdk.SetDefaultTextMapPropagator(new AWSXRayPropagator());
+            });
+            //.WithMetrics(builder =>
+            //{
+            //    builder
+            //        .AddMeter(Instrumentation.MeterName)
+            //        .AddRuntimeInstrumentation()
+            //        .AddHttpClientInstrumentation()
+            //        .AddAspNetCoreInstrumentation();
+
+            //    builder.AddView(instrument =>
+            //    {
+            //        return instrument.GetType().GetGenericTypeDefinition() == typeof(Histogram<>)
+            //            ? new Base2ExponentialBucketHistogramConfiguration()
+            //            : null;
+            //    });
+
+            //    builder.AddOtlpExporter(otlpOptions =>
+            //    {
+            //        // Use IConfiguration directly for Otlp exporter endpoint option.
+            //        otlpOptions.Endpoint = collectorEndpoint;
+            //    });
+            //});
+
+        // Configure OpenTelemetry Logging.
+        appBuilder.Logging.AddOpenTelemetry(options =>
+        {
+            // Note: See appsettings.json Logging:OpenTelemetry section for configuration.
+
+            var resourceBuilder = ResourceBuilder.CreateDefault();
+            configureResource(resourceBuilder);
+            options.SetResourceBuilder(resourceBuilder);
+            options.AddOtlpExporter(otlpOptions =>
+            {
+                // Use IConfiguration directly for Otlp exporter endpoint option.
+                otlpOptions.Endpoint = collectorEndpoint;
+            });
+        });
+
+        appBuilder.Services.AddControllers();
+
+        var app = appBuilder.Build();
+
+        // Configure the HTTP request pipeline.
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
